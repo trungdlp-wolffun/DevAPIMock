09be949c2acbc542a40350e34fa25ea879c1d269
hoaiphuc <phucnhwolffungame@gmail.com>
2023-05-24 18:47:38 +0700

    ok

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 ```sh
 mitmweb --mode reverse:https://data-rivals.staging.thetanarena.com -s open_box.py
 ```
+
+### Server URL:
+`http://localhost:8080/api/v1`
+
+
+### On Windows:
+```sh
+"C:\Program Files\mitmproxy\bin\mitmweb" --mode reverse:https://data-rivals.staging.thetanarena.com -s open_box.py
+```
--- open_box.py
+++ open_box.py
@@ -4,11 +4,12 @@ import json
 
 def response(flow: http.HTTPFlow) -> None:
     print(flow.request.pretty_url)
-    if flow.request.pretty_url.endswith("/rivals-box/view-box"):
+    if flow.request.pretty_url.endswith("/rivals-box/view-box?boxType=3"):
         data = json.loads(flow.response.get_text())
 
-        if (len(data["data"]) >= 1):
-            data["data"][0]["data"]["kind"] = 5
-            data["data"][0]["data"]["type"] = 500
+        if (len(data["data"]["items"]) > 0):
+            data["data"]["items"][0]["data"]["kind"] = 5
+            data["data"]["items"][0]["data"]["type"] = 201
+            data["data"]["items"][0]["kind"] = 5
 
         flow.response.text = json.dumps(data)
